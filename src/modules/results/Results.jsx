2018822139b494f3c4a59d75d3086a25cf6f14d2
lhommenul <antoinejezequel@protonmail.com>
2021-07-16 16:38:37 +0200

    update requests

--- src/modules/results/Results.jsx
+++ src/modules/results/Results.jsx
@@ -1,11 +1,25 @@
-import {React,useState} from 'react'
+import {React,useState,useEffect} from 'react'
 import Result from './result/Result'
 import {EventEmitter} from "../event/index"
+import {reqFromApi}from "../request/req"
 const Results = (props) => {
-    const [list_res, setstate] = useState([]);
-    EventEmitter.subscribe("getResultsFromApi",data=>{
-        setstate(data.data["artists"]);
-    })
+    let [list_res, setstate] = useState([]);
+    useEffect(() => {
+        EventEmitter.subscribe("getResultsFromApi",res=>{
+            setstate( list_res = [
+                'artists',
+                'releases',
+                'recordings',
+            ]
+            .map(value=>{
+                if (res.data[value]!= undefined) {
+                    return res.data[value];
+                }   
+            }))
+            console.log(list_res);
+        })
+        
+    }, [])
     return (
         <section>
             {/* MESSAGE */}
@@ -23,6 +37,7 @@ const Results = (props) => {
             {/* LIST ALL RESULTS */}
             <ul className="list_res">
                 {list_res.map((e,i)=>{
+                    console.log(e);
                     return <Result key={i} data={e} index={i}></Result>
                 })}
             </ul>
