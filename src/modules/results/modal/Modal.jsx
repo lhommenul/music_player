04eacbe22a047568cfa7d19a20d3d68221498629
lhommenul <idlhommenul@gmail.com>
2021-08-27 11:55:04 +0200

    image loading is working

--- src/modules/results/modal/Modal.jsx
+++ src/modules/results/modal/Modal.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import "./css/style.css";
 import { EventEmitter} from "../../event/index"
-import { useEffect,useState } from 'react';
+import { useState } from 'react';
 import Images from "./informations/Images"
-import { reqArtistById,reqAlbumById,reqTitleById,reqCoverReleaseById} from '../../request/req'
 
 const Modal = () => {
     let modal_is_open = false;
@@ -11,33 +10,36 @@ const Modal = () => {
     EventEmitter.subscribe('displayModal',(modal_props_data)=>{
         setModalData({ ...modal_data, ...modal_props_data }) // set data for the modal
         setModalState() // display or dont display the modal
-    });
-    useEffect(() => {
-        // Close Modal on button click
-        document.querySelector(".modal_btn").addEventListener('click',e=>{
-            setModalState()  
+        document.querySelector(".modal_btn").addEventListener('click',()=>{
+            setModalState(); // open or close the modal
         })
-    }, []);
+    });
     function setModalState() {
+        // open or close the modal
         modal_is_open = !modal_is_open;
-        // Modify modal class's => display / do not display
-        let i = document.querySelector("#modal_window");
+        const modal_window = document.querySelector("#modal_window");
         if (modal_is_open) {
-            i.classList.add("modal")
-            i.classList.remove("display_none")
+            modal_window.classList.add("modal")
+            modal_window.classList.remove("display_none")
+            document.addEventListener("keydown",e=>{
+                //close modal
+                if (e.key === "Escape" && modal_is_open) setModalState();
+            })
         } else {
-            i.classList.add("display_none")
+            setModalData({})
+            modal_window.classList.add("display_none")
         }
     }
     return (
         <div id="modal_window" className={"display_none"} tabIndex="1" aria-modal="true">
             <button className="modal_btn">x</button>
             <div className="modal_content">
-                <h1>{modal_data.title}</h1>
+                <h1>{modal_data?.title}</h1>
+                {console.log(modal_data)}
                 <h2>Informations</h2>
                 <h2>Cover Arts</h2>
                 <Images
-                    
+                    images={modal_data?.release}
                 >
                 </Images>
             </div>
--- src/modules/results/modal/informations/Images.jsx
+++ src/modules/results/modal/informations/Images.jsx
@@ -1,14 +1,38 @@
-import {React,useEffect} from 'react'
+import "../css/images.css"
+import {React,useEffect,useState} from 'react'
+import {reqCoverReleaseById} from '../../../request/req'
 const Images = (props) => {
+    const [list_images, setListImages] = useState([])
     useEffect(() => {
-        console.log(props.images);
-        return () => {
-            
-        };
+        if (props.images) {
+            requestCoverImages(props.images)
+        }else{ // waiting for data
+            setListImages(list_images=>[])
+            console.error("waiting for data");
+        }
     }, [props.images]);
+    function requestCoverImages(list_recordings) {
+        list_recordings?.forEach((recording)=>{
+            reqCoverReleaseById(recording.id)
+            .then(more_info=>{
+                more_info?.data?.images.forEach(image => {
+                    setListImages(list_images => list_images.concat([image.thumbnails.small]))
+                });
+            })
+            .catch(err=>{
+                console.error(err);
+            })
+        })
+    }
     return (
-        <div>
-        </div>
+        <ul className="list_images">
+            <li className="title_images">Total Count : {list_images?.length}</li>
+            {
+                list_images.map(e=>{
+                    return <li><img src={e} className="image" alt="image cover album" loading="lazy"/></li>
+                })
+            }
+        </ul>
     )
 }
-export default Images
+export default Images;
