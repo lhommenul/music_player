0ffef10ac7d33c9719436939f8dc305bf0794849
lhommenul <idlhommenul@gmail.com>
2021-09-10 09:38:39 +0200

    update package

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // ==== MODULES ====
-import {reqArtist} from './Request.js'
 import SearchBar from './SearchBar.js'
 import Results from './Results.js'
 
 const container_results = new Results();
 
-const search_bar = new SearchBar(container_results);
+new SearchBar(container_results);
